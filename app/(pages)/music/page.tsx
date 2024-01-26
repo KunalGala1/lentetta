a3c5f6099eb3c9b6d84a41c8ff1ed28395247c79
Kunal Gala <kunal.gala16@gmail.com>
2024-01-25 19:52:26 -0500

    do not auto cap music titles

--- app/(pages)/music/page.tsx
+++ app/(pages)/music/page.tsx
@@ -1,10 +1,10 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { getRecordings } from '@/sanity/sanity.query';
-import type { RecordingType } from '@/types';
+"use client";
+import { useState, useEffect } from "react";
+import { getRecordings } from "@/sanity/sanity.query";
+import type { RecordingType } from "@/types";
 
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React from "react";
+import ReactPlayer from "react-player";
 
 const MusicPage = () => {
   const [recordings, setRecordings] = useState<RecordingType[] | null>(null);
@@ -28,7 +28,7 @@ const MusicPage = () => {
   }, []);
 
   return (
-    <div className='p-4 space-y-4 md:space-y-0 md:flex md:items-start md:gap-6 md:justify-center md:flex-wrap max-w-[1700px] mx-auto'>
+    <div className="p-4 space-y-4 md:space-y-0 md:flex md:items-start md:gap-6 md:justify-center md:flex-wrap max-w-[1700px] mx-auto">
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
 
@@ -36,18 +36,16 @@ const MusicPage = () => {
         recordings.map((recording) => (
           <div
             key={recording._id}
-            className='p-4 bg-slate-200/60 rounded flex-1 sm:flex-[45%] lg:flex-[30%] min-w-[25%] max-w-full'
+            className="p-4 bg-slate-200/60 rounded flex-1 sm:flex-[45%] lg:flex-[30%] min-w-[25%] max-w-full"
           >
-            <div className='py-4'>
-              <h2 className='font-bold text-xl capitalize'>
-                {recording.title}
-              </h2>
-              <p className='text-gray-950/50'>{recording.subtitle}</p>
+            <div className="py-4">
+              <h2 className="font-bold text-xl">{recording.title}</h2>
+              <p className="text-gray-950/50">{recording.subtitle}</p>
             </div>
-            <div className='mt-4'>
+            <div className="mt-4">
               <ReactPlayer
                 url={recording.url}
-                width={'100%'}
+                width={"100%"}
                 height={300}
                 controls={true}
               />
