019366bf1bea272a9de75cf62b64d8554da85a19
Kunal Gala <kunal.gala16@gmail.com>
2024-05-22 11:00:28 -0400

    feat: added favicon and readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from 'next';
-import { Sorts_Mill_Goudy } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Sorts_Mill_Goudy } from "next/font/google";
+import "./globals.css";
 
 const font = Sorts_Mill_Goudy({
-  weight: '400',
-  subsets: ['latin'],
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Len Tetta | Composer',
-  description: 'Len Tetta is a composer based in Boston.',
+  title: "Len Tetta | Composer",
+  description: "Len Tetta is a composer based in Boston.",
 };
 
 export default function RootLayout({
@@ -18,10 +18,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
+      <>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </>
+
       <body
         className={
-          font.className + ' bg-slate-50 text-slate-950 leading-relaxed'
+          font.className + " bg-slate-50 text-slate-950 leading-relaxed"
         }
       >
         {children}
